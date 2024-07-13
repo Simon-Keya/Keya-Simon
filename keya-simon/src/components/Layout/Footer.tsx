c151e7b05c0b533f8f1423759b4f5ab84fb7cb3a
Simon Keya <120686819+Simon-Keya@users.noreply.github.com>
2024-07-13 21:10:23 +0300

    commit_12

--- keya-simon/src/components/Layout/Footer.tsx
+++ keya-simon/src/components/Layout/Footer.tsx
@@ -1,41 +1,78 @@
-import React from 'react';
-import { FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa';
+import { FaFacebook, FaGithub, FaInstagram, FaLinkedin, FaTwitter } from 'react-icons/fa';
 import '../styles/Layout/Footer.css';
+
+const SITEMAP = [
+  {
+    title: "Company",
+    links: ["About Us", "Careers", "Our Team", "Projects"],
+  },
+  {
+    title: "Help Center",
+    links: ["Discord", "Twitter", "GitHub", "Contact Us"],
+  },
+  {
+    title: "Resources",
+    links: ["Blog", "Newsletter", "Free Products", "Affiliate Program"],
+  },
+  {
+    title: "Products",
+    links: ["Templates", "UI Kits", "Icons", "Mockups"],
+  },
+];
+
+const currentYear = new Date().getFullYear();
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white py-6">
-      <div className="container mx-auto flex justify-between items-center px-4">
-        {/* Social Media Icons */}
-        <div className="flex items-center space-x-4">
-          <button onClick={() => window.open('https://twitter.com')} className="text-white focus:outline-none">
-            <FaTwitter className="h-6 w-6 fill-current" />
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-grid">
+          {SITEMAP.map(({ title, links }, index) => (
+            <div key={index} className="footer-column">
+              <h5>{title}</h5>
+              <ul>
+                {links.map((link, index) => (
+                  <li key={index}>
+                    <a href="#" className="text-sm hover:underline">
+                      {link}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+        <div className="footer-social-icons">
+          <button onClick={() => window.open('https://twitter.com')} aria-label="Twitter">
+            <FaTwitter className="footer-icon" />
           </button>
-          <button onClick={() => window.open('https://instagram.com')} className="text-white focus:outline-none">
-            <FaInstagram className="h-6 w-6 fill-current" />
+          <button onClick={() => window.open('https://instagram.com')} aria-label="Instagram">
+            <FaInstagram className="footer-icon" />
           </button>
-          <button onClick={() => window.open('https://linkedin.com')} className="text-white focus:outline-none">
-            <FaLinkedin className="h-6 w-6 fill-current" />
+          <button onClick={() => window.open('https://linkedin.com')} aria-label="LinkedIn">
+            <FaLinkedin className="footer-icon" />
+          </button>
+          <button onClick={() => window.open('https://github.com')} aria-label="GitHub">
+            <FaGithub className="footer-icon" />
+          </button>
+          <button onClick={() => window.open('https://facebook.com')} aria-label="Facebook">
+            <FaFacebook className="footer-icon" />
           </button>
         </div>
-        {/* Privacy Terms and Policy */}
-        <div className="flex items-center space-x-4">
-          <button onClick={() => window.open('/privacytermspolicy')} className="text-sm text-white hover:underline focus:outline-none">
+        <div className="footer-legal">
+          <button onClick={() => window.open('/privacytermspolicy')} aria-label="Privacy Policy">
             Privacy Policy
           </button>
-          <button onClick={() => window.open('/privacytermspolicy')} className="text-sm text-white hover:underline focus:outline-none">
+          <button onClick={() => window.open('/termsandconditions')} aria-label="Terms & Conditions">
             Terms & Conditions
           </button>
         </div>
-      </div>
-      {/* Copyright Information */}
-      <div className="container mx-auto text-center mt-4">
-        <p className="text-sm">
-          &copy; {new Date().getFullYear()} Keya Simon. All Rights Reserved.
-        </p>
+        <div className="footer-copy">
+          <p>&copy; {currentYear} Keya Simon. All Rights Reserved.</p>
+        </div>
       </div>
     </footer>
   );
-};
+}
 
 export default Footer;
--- keya-simon/src/components/Layout/Header.tsx
+++ keya-simon/src/components/Layout/Header.tsx
@@ -1,67 +1,88 @@
-import React, { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Layout/Header.css';
 
 const Header = () => {
-  const [isAccountDropdownOpen, setIsAccountDropdownOpen] = useState(false);
+  const [isResourcesDropdownOpen, setIsResourcesDropdownOpen] = useState(false);
   const [isCategoriesDropdownOpen, setIsCategoriesDropdownOpen] = useState(false);
+  const resourcesDropdownRef = useRef<HTMLDivElement>(null);
+  const categoriesDropdownRef = useRef<HTMLDivElement>(null);
 
-  const toggleAccountDropdown = () => {
-    setIsAccountDropdownOpen(!isAccountDropdownOpen);
+  const toggleResourcesDropdown = () => {
+    setIsResourcesDropdownOpen(!isResourcesDropdownOpen);
   };
 
   const toggleCategoriesDropdown = () => {
     setIsCategoriesDropdownOpen(!isCategoriesDropdownOpen);
   };
 
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      resourcesDropdownRef.current && !resourcesDropdownRef.current.contains(event.target as Node) &&
+      categoriesDropdownRef.current && !categoriesDropdownRef.current.contains(event.target as Node)
+    ) {
+      setIsResourcesDropdownOpen(false);
+      setIsCategoriesDropdownOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   return (
-    <header className="bg-blue-600 text-white py-4">
-      <div className="container mx-auto ">
-        {/* Logo and Title */}
-        <Link to="/" className="flex items-center text-white no-underline">
-          <img src="/logo.png" alt="Logo" className="h-8 mr-2" />
-          <span className="font-bold text-xl">Simon Keya</span>
+    <header>
+      <div className="header-container">
+        <Link to="/" className="header-logo">
+          <img src="/logo.png" alt="Logo" />
+          <span>Simon Keya</span>
         </Link>
 
-        {/* Navigation Links */}
-        <nav className="flex">
-          <Link to="/" className="px-4 py-2 hover:text-gray-300">Home</Link>
-          <Link to="/blog" className="px-4 py-2 hover:text-gray-300">Blog</Link>
-
-          {/* Account Dropdown */}
-          <div className="relative px-4 py-2">
-            <button onMouseEnter={toggleAccountDropdown} onMouseLeave={toggleAccountDropdown}>
-              Account
-            </button>
-            {isAccountDropdownOpen && (
-              <div className="origin-top-right absolute right- mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5">
-                <Link to="/profile" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Profile</Link>
-                <Link to="/settings" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Settings</Link>
-              </div>
-            )}
+        <nav className="header-nav">
+          <div className="center-links">
+            <Link to="/">Home</Link>
+            <Link to="/blog">Blog</Link>
+            <div className="dropdown-menu" ref={categoriesDropdownRef}>
+              <button
+                className="dropdown-button"
+                onClick={toggleCategoriesDropdown}
+              >
+                Categories
+              </button>
+              {isCategoriesDropdownOpen && (
+                <div className="dropdown-content">
+                  <Link to="/art">Art</Link>
+                  <Link to="/psychology">Psychology</Link>
+                  <Link to="/technology">Technology</Link>
+                  <Link to="/philosophy">Philosophy</Link>
+                  <Link to="/self-improvement">Self Improvement</Link>
+                </div>
+              )}
+            </div>
+            <div className="dropdown-menu" ref={resourcesDropdownRef}>
+              <button
+                className="dropdown-button"
+                onClick={toggleResourcesDropdown}
+              >
+                Resources
+              </button>
+              {isResourcesDropdownOpen && (
+                <div className="dropdown-content">
+                  <Link to="/books">Books</Link>
+                  <Link to="/news">News</Link>
+                  <Link to="/tutorials">Tutorials</Link>
+                  <Link to="/support">Support</Link>
+                </div>
+              )}
+            </div>
           </div>
-
-          {/* Categories Dropdown */}
-          <div className="relative px-4 py-2">
-            <button onMouseEnter={toggleCategoriesDropdown} onMouseLeave={toggleCategoriesDropdown}>
-              Categories
-            </button>
-            {isCategoriesDropdownOpen && (
-              <div className="origin-top-right absolute right- mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5">
-                {/* Map over your categories if they are dynamic otherwise list them as below */}
-                <Link to="/Art" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Art</Link>
-                <Link to="/Psychology" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Psychology</Link>
-                <Link to="/Technology" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Technology</Link>
-                <Link to="/Philosophy" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Philosophy</Link>
-                <Link to="/self improvement" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Self Improvement</Link>
-                {/* ... Other categories */}
-              </div>
-            )}
+          <div className="auth-links">
+            <Link to="/login" className="login-button">Login</Link>
+            <Link to="/register" className="signup-button">Sign Up</Link>
           </div>
-
-          {/* Authentication Links */}
-          <Link to="/login" className="px-4 py-2 hover:text-gray-300">Login</Link>
-          <Link to="/register" className="px-4 py-2 hover:text-gray-300">Sign Up</Link>
         </nav>
       </div>
     </header>
